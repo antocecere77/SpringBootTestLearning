9aa85a1688eb190e54dad12cbc8456fc9d6f200a
antocecere <antocecere77@gmail.com>
2020-12-14 06:43:34 +0100

    Value Injection Using Setter Methods

--- src/test/java/com/antocecere77/springselenium/Address.java
+++ src/test/java/com/antocecere77/springselenium/Address.java
@@ -1,15 +1,13 @@
 package com.antocecere77.springselenium;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Data
 @Component
 public class Address {
 
+    @Value("${street}")
     private String street;
-
-    public Address() {
-        this.street = "123 non main st";
-    }
 }
--- src/test/java/com/antocecere77/springselenium/Salary.java
+++ src/test/java/com/antocecere77/springselenium/Salary.java
@@ -1,17 +1,13 @@
 package com.antocecere77.springselenium;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Data
 @Component
 public class Salary {
 
+    @Value("${amount:500}")
     int amount;
-
-    public Salary() {
-        this.amount = 1000;
-    }
-
-
 }
