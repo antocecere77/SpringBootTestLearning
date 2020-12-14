7e78bf406ee0c576bc911e1ca275d00bd9996aff
antocecere <antocecere77@gmail.com>
2020-12-14 06:35:00 +0100

    Value injection demo

--- src/test/java/com/antocecere77/springselenium/SpringSeleniumApplicationTests.java
+++ src/test/java/com/antocecere77/springselenium/SpringSeleniumApplicationTests.java
@@ -2,7 +2,10 @@ package com.antocecere77.springselenium;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class SpringSeleniumApplicationTests {
@@ -10,8 +13,21 @@ class SpringSeleniumApplicationTests {
 	@Autowired
 	private User user;
 
+	@Value("${Path}")
+	private String path;
+
+	@Value("${fruits}")
+	private List<String> fruits;
+
+	@Value("${timeout}")
+	private Integer timeout;
+
 	@Test
 	void contextLoads() {
+		System.out.println(path);
+		System.out.println(fruits);
+		System.out.println(fruits.size());
+		System.out.println(timeout);
 		user.printDetails();
 	}
 
