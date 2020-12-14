7f67dfeced3b7fbc64a4de139edb5e141dc4032b
antocecere <antocecere77@gmail.com>
2020-12-14 08:02:33 +0100

    Chromedriver bean

--- src/main/java/com/antocecere77/springselenium/config/WebDriverConfig.java
+++ src/main/java/com/antocecere77/springselenium/config/WebDriverConfig.java
@@ -2,6 +2,7 @@ package com.antocecere77.springselenium.config;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +11,7 @@ public class WebDriverConfig {
 
     @Bean
     public WebDriver chromeDriver() {
-        WebDriverManager.chromedriver().setup();
+        WebDriverManager.chromedriver().version("87.0.4280.88").setup();
+        return new ChromeDriver();
     }
 }
